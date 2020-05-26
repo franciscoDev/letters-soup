b42631b763ffba7f9c477669c891f621c1b990b7
franciscoDev <franciscoDev>
2020-05-25 23:04:34 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { Soup } from "./soup";
 
-let soup = new Soup( [
-    "paja",
-    "felin",
-    "peces",
-    "sofa",
-    "tele",
-    "oso"
-  ],10);
- 
+let soup = new Soup( ["paja","felin","peces","sofa","tele","oso"],10);
 console.log(soup.generateSoup());
- 
+//TODO
+// 1: invertir la palabra.
+// 2: isertar palabras en diagonal.
+// 3: solapar palabras que tengas letras en comun.
